20df5cd8aa18a0994343ebca8051ea0ed17fca75
George Eracleous <basketballcy@gmail.com>
2012-04-01 11:01:08 +0100

    Randomization button and rankings

--- app/handlers/rankings.py
+++ app/handlers/rankings.py
@@ -3,13 +3,12 @@ from mongoengine.queryset import DoesNotExist
 from app.model.video import *
 from random import shuffle
 
-class HomePageHandler(base.BaseHandler):
+class ViewRankingsHandler(base.BaseHandler):
     '''
     Check user status and either load the home screen or the
     welcome page.
     '''
     def on_get(self):
-        print 'Hello'
         songs = [item for item in VideoItem.objects if not item.viewed]
         shuffle(songs)
         first_selection = songs.pop(0)
@@ -23,14 +22,5 @@ class HomePageHandler(base.BaseHandler):
         return first_selection, second_selection
     
     def on_success(self, f, s):
-        self.base_render("intro.html", first=f, second=s)
-    
-class VoteHandler(base.BaseHandler):
-
-    def on_post(self):
-        sid = self.get_argument("sid", None)
-        vi = VideoItem.objects(id=sid).get()
-        vi.upvote()
-        
-        
+        self.base_render("rankings.html", first=f, second=s)
     
--- urls.py
+++ urls.py
@@ -3,11 +3,13 @@
 # Author: Giorgos Eracleous #
 #############################
 
-from app.handlers import base, home, admin
+from app.handlers import base, home, admin, rankings
 
 url_patterns = [
     ("/", home.HomePageHandler),
+    ("/next", home.HomePageHandler),
     ("/vote", home.VoteHandler),
+    ("/rankings", rankings.ViewRankingsHandler),
     ##########ADMIN HANDLERS#################
     ("/populate", admin.PopulateDummyHandler),
 ]
